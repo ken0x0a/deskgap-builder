3dcbb5ca84fb1634ce0c6c2233d11b3282a001a5
Ken <2770219+ken0x0a@users.noreply.github.com>
2020-04-19 13:13:52 +0900

    chore: add lint related deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,50 @@
+/**
+ * [docs](@ken0x0a/eslint-config/docs/example.md)
+ */
 module.exports = {
+  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint",
-  ],
+
+  parserOptions: {
+    project: "tsconfig.js.json",
+  },
+
+  /**
+   * NOTE: The **order is important**, to properly override rules by later one
+   */
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    "@ken0x0a/eslint-config/base",
+    "@ken0x0a/eslint-config/tslint-rules",
+    "@ken0x0a/eslint-config/import",
+    "@ken0x0a/eslint-config/jest",
+    /**
+     * if you need to use `react` related configs like following,
+     * you need to install optional dependency "@ken0x0a/eslint-config-react-deps" by
+     * `yarn add -D "@ken0x0a/eslint-config-react-deps"`
+     */
+    // '@ken0x0a/eslint-config/react',
+    // '@ken0x0a/eslint-config/react-native',
+    // '@ken0x0a/eslint-config/expo', // extends "./react-native"
+
+    /**
+     * '@ken0x0a/eslint-config/typescript' must be always last, to override rules
+     */
+    "@ken0x0a/eslint-config/typescript",
   ],
   rules: {
-    semi: "off",
-    "@typescript-eslint/member-delimiter-style": ["error", {
-      multiline: {
-        delimiter: "none",
+    /**
+     * specific
+     */
+    "@typescript-eslint/camelcase": [2, { properties: "never", ignoreDestructuring: false }],
+  },
+  overrides: [
+    {
+      // for JS files
+      files: ["**/*.js"],
+      parserOptions: {
+        project: "tsconfig.js.json",
       },
-    }],
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "no-constant-condition": "off",
-    "@typescript-eslint/no-inferrable-types": "off",
-    "@typescript-eslint/explicit-function-return-type": ["off", {
-      // "allowExpressions": false,
-    }]
-  }
+    },
+  ],
+  globals: {},
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  ...require("@ken0x0a/prettier-config"),
   printWidth: 120,
   semi: false,
-};
+}
